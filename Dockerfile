196b5073a7d56cfdccc8786e95bc5b3c8c5c1a1a
Shubham2786 <ghostridersarwar2017@gmail.com>
2026-04-17 03:01:36 +0530

    ecr features added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 #stage 1 of the dockerfile
-FROM node:20-slim AS builder
+FROM public.ecr.aws/docker/library/node:20-slim AS builder
 
 WORKDIR /app
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# script to start the application
+
 cd /home/ec2-user/forecastify
-# Build image manually to avoid Buildx compatibility issues with Compose
-docker load < forecastify.tar
-# Start the container
+
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 360121241545.dkr.ecr.eu-west-1.amazonaws.com
+
+docker pull 360121241545.dkr.ecr.eu-west-1.amazonaws.com/forecastify:latest
+
+docker compose down || true
 docker compose up -d
