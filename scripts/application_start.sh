9766f7571788500971544b8ecc25254b9275079e
Shubham2786 <ghostridersarwar2017@gmail.com>
2026-04-17 01:59:04 +0530

    my changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,6 @@
 # script to start the application
 cd /home/ec2-user/forecastify
 # Build image manually to avoid Buildx compatibility issues with Compose
-docker build -t forecastify:latest .
+docker load < forecastify.tar
 # Start the container
 docker compose up -d
