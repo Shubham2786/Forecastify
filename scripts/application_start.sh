9a6a4b7f75ecc8d0c08b1bf30db60aa9e4c0b7cf
Shubham2786 <ghostridersarwar2017@gmail.com>
2026-04-23 02:59:34 +0530

    made changes for IaC

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,18 +3,19 @@ set -e
 
 cd /home/ec2-user/forecastify
 
-# Fetch secrets from SSM
-export NEXT_PUBLIC_SUPABASE_URL=$(aws ssm get-parameter --name "/forecastify/dev/NEXT_PUBLIC_SUPABASE_URL" --with-decryption --query "Parameter.Value" --output text)
-export NEXT_PUBLIC_SUPABASE_ANON_KEY=$(aws ssm get-parameter --name "/forecastify/dev/NEXT_PUBLIC_SUPABASE_ANON_KEY" --with-decryption --query "Parameter.Value" --output text)
-export OPENWEATHER_API_KEY=$(aws ssm get-parameter --name "/forecastify/dev/OPENWEATHER_API_KEY" --with-decryption --query "Parameter.Value" --output text)
-export GROQ_API_KEY=$(aws ssm get-parameter --name "/forecastify/dev/GROQ_API_KEY" --with-decryption --query "Parameter.Value" --output text)
-export GROQ_API_KEY_2=$(aws ssm get-parameter --name "/forecastify/dev/GROQ_API_KEY_2" --with-decryption --query "Parameter.Value" --output text)
-export GROQ_API_KEY_3=$(aws ssm get-parameter --name "/forecastify/dev/GROQ_API_KEY_3" --with-decryption --query "Parameter.Value" --output text)
-export SERPER_API_KEY=$(aws ssm get-parameter --name "/forecastify/dev/SERPER_API_KEY" --with-decryption --query "Parameter.Value" --output text)
-export GOOGLE_MAPS_API_KEY=$(aws ssm get-parameter --name "/forecastify/dev/GOOGLE_MAPS_API_KEY" --with-decryption --query "Parameter.Value" --output text)
+echo "Fetching parameters from SSM..."
+aws ssm get-parameters-by-path \
+  --path "/forecastify/dev/" \
+  --with-decryption \
+  --region $REGION \
+  --query "Parameters[*].[Name,Value]" \
+  --output text | while read name value; do
+    key=$(basename $name)
+    echo "$key=$value" >> .env
+done
 
 # Login + pull + run
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 360121241545.dkr.ecr.eu-west-1.amazonaws.com
-docker pull 360121241545.dkr.ecr.eu-west-1.amazonaws.com/forecastify:latest
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 950110265938.dkr.ecr.eu-west-1.amazonaws.com
+docker pull 950110265938.dkr.ecr.eu-west-1.amazonaws.com/forecastify-dev-repo:latest
 docker-compose down || true
 docker-compose up -d
